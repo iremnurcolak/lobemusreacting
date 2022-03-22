8d6d0e7b1670dbac9018f5de067193d0e4727d36
iremnurcolak <iremnurcolak34@gmail.com>
2022-03-22 15:51:12 +0300

    Added state to change title when button clicked.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,29 @@
-import react from "react";
+import react, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  //function ExpenseItem(props) {}
+
+  const [title, setTitle] = useState(props.title);
+  //title: current state element, setTitle: function for updating title
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
