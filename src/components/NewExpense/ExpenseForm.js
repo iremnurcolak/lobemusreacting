39d79e362249acdb523af715d1d0aa876bedeef2
iremnurcolak <iremnurcolak34@gmail.com>
2022-03-25 11:08:14 +0300

    Added submit handler.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -50,9 +50,19 @@ const ExpenseForm = () => {
     //   return { ...prevState, enteredDate: event.target.value };
     // });
   };
+  const submitHandler = (event) => {
+    //prevent reloading page when submitted
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+  };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
