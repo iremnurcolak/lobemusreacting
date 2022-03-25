c1fbb43c6fb3cec03b9885984f6f9df82370cdfe
iremnurcolak <iremnurcolak34@gmail.com>
2022-03-25 11:00:21 +0300

    Added multiple states to track form input changes.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,10 +1,54 @@
-import react from "react";
+import react, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  //with multiple states
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  //with one state
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
+
   const titleChangeHandler = (event) => {
     //prints user title input
-    console.log(event.target.value);
+    //console.log(event.target.value);
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   //copying existing values to not lose
+    //   ...userInput,
+    //   //overwrite
+    //   enteredTitle: event.target.value,
+    // });
+
+    //more safer way
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //  ...userInput,
+    //  enteredAmount: event.target.value,
+    // });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredAmount: event.target.value };
+    // });
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //  ...userInput,
+    //  enteredDate: event.target.value,
+    // });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredDate: event.target.value };
+    // });
   };
 
   return (
@@ -16,11 +60,21 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2021-01-01" max="2022-03-25" />
+          <input
+            type="date"
+            min="2021-01-01"
+            max="2022-03-25"
+            onChange={dateChangeHandler}
+          />
         </div>
         <div className="new-expense__actions">
           <button type="submit">Add Expense</button>
