56c75c31ce5d75085e93cfeb81e2b8737007c0ff
iremnurcolak <iremnurcolak34@gmail.com>
2022-04-04 08:49:30 +0300

    Added bar chart.

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -3,12 +3,21 @@ import react from "react";
 import "./ChartBar.css";
 
 const ChartBar = (props) => {
+  let barFillHeight = "0%";
+
+  if (props.maxValue > 0) {
+    console.log(props.value / props.maxValue);
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
-        <div className="chart-bar__fill"></div>
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
       </div>
-      <div className="chart-bar__label"></div>
+      <div className="chart-bar__label">{props.label}</div>
     </div>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -23,6 +24,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onFilterChange={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
         {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
         {filteredExpenses.length > 0 &&
